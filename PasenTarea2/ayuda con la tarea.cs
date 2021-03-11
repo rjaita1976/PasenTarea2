56498efcdde2d9dafeb3d0ee5e767c8e70a9b629
jaita52 <rogerjaita52@gmail.com>
2021-03-11 16:56:26 -0400

    Agregar archivos de proyecto.

---
+++ PasenTarea2/ayuda con la tarea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasenTarea2
+{
+    class ayuda_con_la_tarea
+    {
+        //flaco pasa la tarea
+    }
+}
